ca8dc1e4d50cb5e16272fddd526bcd094b394690
Jonas Almeida <jalmeida@mathbiol.org>
2013-09-19 17:04:34 -0500

    using DataChannel https://github.com/muaz-khan/WebRTC-Experiment/tree/master/DataChannel

--- evalMachine.js
+++ evalMachine.js
@@ -1,1 +1,56 @@
 console.log('evalMachine loaded');
+
+evalMachine={
+
+	uid:function(prefix){
+		if(!prefix){prefix='UID'}
+		var uid=prefix+Math.random().toString().slice(2);
+		return uid
+	},
+
+	load:function(url,cb,er){ // load script / JSON
+		var s = document.createElement('script');
+		s.src=url;
+		s.id = this.uid();
+		if(!!cb){s.onload=cb}
+		if(!!er){s.onerror=er}
+		document.body.appendChild(s);
+		setTimeout('document.body.removeChild(document.getElementById("'+s.id+'"));',10000); // is the waiting still needed ?
+		return s.id
+	},
+
+	build:function(){
+		// build eval machine here
+		console.log('building eval machine ...')
+		this.connect=function(id){
+
+		}
+
+	}
+};
+
+
+
+if(!window.DataChannel){ // make sure DataChennal is loaded first
+	evalMachine.load('https://www.webrtc-experiment.com/DataChannel.js',function(){
+		evalMachine.build();
+	})
+} else {
+	evalMachine.build();
+}
+
+
+
+
+/*
+// make sure PUBNUB libraries are loaded
+if(!window.PUBNUB){
+	evalMachine.load('https://cdn.pubnub.com/pubnub.min.js',function(){
+		evalMachine.load('https://rawgithub.com/pubnub/webrtc/master/src/webrtc-beta-pubnub.js',function(){
+			evalMachine.build();
+		})
+	})
+} else {
+	evalMachine.build();
+}
+*/
