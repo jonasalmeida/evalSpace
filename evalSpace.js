ff523266440c35bf21c5d0240a07d3b8437307d4
Jonas Almeida <jalmeida@mathbiol.org>
2013-09-23 22:40:39 -0500

    EvalSPace instantiable

--- evalSpace.js
+++ evalSpace.js
@@ -1,16 +1,36 @@
-console.log('evalSpace loaded');
+console.log('EvalSpace loaded');
 
-evalSpace={
+// Make load PeerJS first
 
-	consoleLog:true,
+EvalSpace=function(url,cb,er){ // load script / JSON
+	var s = document.createElement('script');
+	s.src=url;
+	s.id = 'UID'+Math.random().toString().slice(2);
+	if(!!cb){s.onload=cb}
+	if(!!er){s.onerror=er}
+	document.body.appendChild(s);
+	setTimeout('document.body.removeChild(document.getElementById("'+s.id+'"));',10000); // is the waiting still needed ?
+	return s.id
+};
 
-	uid:function(prefix){
+EvalSpace('//cdn.peerjs.com/latest/peer.min.js',
+function(){
+
+// overright EvalSpace with the actual EvalSpace class
+
+EvalSpace=function(APIkey,uid){
+
+this.spaces=[]; // track eval spaces here
+this.consoleLog=true;
+this.APIkey='ime97m2i45bnvcxr'; // defaul APIkey, change it before connecting if you want to use a different one
+
+this.uid=function(prefix){
 		if(!prefix){prefix='UID'}
 		var uid=prefix+Math.random().toString().slice(2);
 		return uid
-	},
+	};
 
-	load:function(url,cb,er){ // load script / JSON
+this.load=function(url,cb,er){ // load script / JSON
 		var s = document.createElement('script');
 		s.src=url;
 		s.id = this.uid();
@@ -19,9 +39,9 @@ evalSpace={
 		document.body.appendChild(s);
 		setTimeout('document.body.removeChild(document.getElementById("'+s.id+'"));',10000); // is the waiting still needed ?
 		return s.id
-	},
+	};
 
-	stringify:function(x){ // extends JSON.stringify to work with both values and functions
+this.stringify=function(x){ // extends JSON.stringify to work with both values and functions
 		var y=typeof(x);
 		switch (y){
 		case 'function':
@@ -51,120 +71,123 @@ evalSpace={
 		default:
 		}
 		return y;
-	},
+	};
 
-	buildThis:function(){
-		// build eval machine here
-		console.log('building eval machine ...');
+this.connect=function(APIkey,uid){
+	// build eval space
+	console.log('building a WebRTC eval space ...');
+	if(!!APIkey){this.APIkey=APIkey};
+	if(!uid){this.id=this.uid('evalSpace')}
+	else{this.id=uid};
+	// create peer
+	this.peer = new Peer(this.id,{key: this.APIkey});
+	// track it
+	if(!EvalSpace.spaces){EvalSpace.spaces={}};
+	EvalSpace.spaces[this.id]=this;
+	// find out if it is a submitter, an evaluator or an error	
+	this.peer.on('open',function(){ // it is a submitter
+		EvalSpace.spaces[this.id].buildSubmiter();
+	})
 
-		// get connection specs
-		this.APIkey='u5ouack7vtkgwrk9'; // defaul APIkey, change it before connecting if you want to use a different one
-		this.connect=function(APIkey,uid){
-			if(!!APIkey){evalSpace.APIkey=APIkey};
-			if(!uid){uid=evalSpace.uid('evalSpace')};
-			evalSpace.id=uid;
-			evalSpace.peer = new Peer(uid,{key: evalSpace.APIkey});
+	this.peer.on('error',function(err){ // find out if this is an evaluator first
+		if(err.type=="unavailable-id"){EvalSpace.spaces[this.id].buildEvaluator()} // it is an evaluator !
+		else{throw('PeerJS error: '+err.type)}
+	})
+	this.callMe='new EvalSpace("'+this.APIkey+'","'+this.id+'")';
+	return this.id;
+};
 
-			// give it a second or two and check if it is connected
-			setTimeout(function(){
-					//console.log('connected: ',evalSpace.peer.isConnected()); // this is assynchronous
-					
-					if(evalSpace.peer.isConnected()){// then this is the SUBMITTER
-						evalSpace.role='submitter';
+this.buildSubmiter=function(){
+	this.role='submitter';
+	console.log('role: ',this.role);
+	// listen to callBacks from evaluators
+	this.peer.on('connection', function(conn) {
+		conn.on('data', function(data){
+			console.log(data.id+' < '+data.Y);
+			//console.log('received: ',data);
+		})
+	});
+	// evaluate function
+	this.evaluate=function(data,evaluator){
+		if(typeof(data)=='string'){
+			data={
+				id:this.uid('task'),
+				X:data
+			}
+		}
+		data.spaceId=this.id; // tracking which space will be dealing with this data
+		// list of evaluators
+		var evs=Object.getOwnPropertyNames(this.peer.connections);	
+		// if evaluator was selected by its position in the list of connections
+		if(typeof(evaluator)=='number'){
+			evaluator=this.peer.connections[evs[evaluator]].peerjs;
+		}
+		// if no evaluator was specified pick one randomly
+		if(!evaluator){
+			if(evs.length==0){error('no connections were found')}
+			else{
+				var i = Math.floor(Math.random()*evs.length);
+				evaluator=this.peer.connections[evs[i]].peerjs;
+			}
+		}
+		// ready to send
+		evaluator.send(data);
 
-						// listen to callBacks from evaluators
-						evalSpace.peer.on('connection', function(conn) {
-							conn.on('data', function(data){
-								console.log(data.id+' < '+data.Y);
-								//console.log('received: ',data);
-							})
-						});
+		if(EvalSpace.spaces[data.spaceId].consoleLog){
+			console.log(data.id+' > '+data.X);
+		}
+		return data.id; // return id of call
+	}
 
+	if(this.consoleLog){
+		console.log('to activate evaluators do: ',this.callMe);
+	}
 
-						// evaluate function
-						evalSpace.evaluate=function(data,evaluator){
-							if(typeof(data)=='string'){
-								data={
-									id:evalSpace.uid('id'),
-									X:data
-								}
-							}
-							// list of evaluators
-							var evs=Object.getOwnPropertyNames(evalSpace.peer.connections);	
-							// if evaluator was selected by its position in the list of connections
-							if(typeof(evaluator)=='number'){
-								evaluator=evalSpace.peer.connections[evs[evaluator]].peerjs;
-							}
-							// if no evaluator was specified pick one randomly
-							if(!evaluator){
-								if(evs.length==0){error('no connections were found')}
-								else{
-									var i = Math.floor(Math.random()*evs.length);
-									evaluator=evalSpace.peer.connections[evs[i]].peerjs;
-								}
-							}
-							// ready to send
-							evaluator.send(data);
+};
 
-							if(consoleLog){
-								console.log(data.id+' > '+data.X);
-							}
-							
-
-							return data.id; // return id of call
-
-						}
-					} 
-					
-					else {
-						evalSpace.role='evaluator';
-						// connect again, now as an evaluator
-						var uid = evalSpace.uid('evaluator');
-						evalSpace.peer = new Peer(uid,{key: evalSpace.APIkey});
-						evalSpace.callBack = evalSpace.peer.connect(evalSpace.id); // give notice to submitter
-						evalSpace.callBack.on('data',function(data){
-							console.log(data.id+' < '+data.X);
-							try {
-								data.Y = eval(data.X);
-								data.err = false;
-							}
-							catch (err){
-								data.A = err;
-								data.err = true;
-							}
-
-							if(consoleLog){
-								console.log(data.id+' > '+data.Y);
-							}
-
-							//console.log('evaled data: ',data);
-							evalSpace.callBack.send(data);
-
-						});
-					};
-					
-					console.log('role: ',evalSpace.role);
-					if(evalSpace.role=='submitter'){
-						if(consoleLog){
-							console.log('Evaluators: evalSpace.connect("'+evalSpace.APIkey+'","'+evalSpace.id+'")');
-						}
-					}
-				}
-			,1000)
-
-			
-			return uid;
+this.buildEvaluator=function(){
+	this.role='evaluator';
+	console.log('role: ',this.role);
+	// connect again, now as an evaluator
+	var uid = this.uid('evaluator');
+	this.peer = new Peer(uid,{key: this.APIkey});
+	this.callBack = this.peer.connect(this.id); // give notice to submitter
+	this.callBack.on('data',function(data){
+		if(EvalSpace.spaces[data.spaceId].consoleLog){
+			console.log(data.id+' < '+data.X);
+		}
+		
+		try {
+			data.Y = eval(data.X);
+			data.err = false;
+		}
+		catch (err){
+			data.A = err;
+			data.err = true;
 		}
 
-	}
+		if(EvalSpace.spaces[data.spaceId].consoleLog){
+			console.log(data.id+' > '+data.Y);
+		}
+
+		//console.log('evaled data: ',data);
+		this.send(data);
+
+	});
+};
+
+this.connect(APIkey,uid);
+
 };
 
 
-
-if(!window.Peer){ // make sure DataChennal is loaded first
-	evalSpace.load('//cdn.peerjs.com/latest/peer.min.js',function(){
-		evalSpace.buildThis();
 	})
-} else {
-	evalSpace.build();
+
+
+//EvalSpace.spaces=[]; // track eval spaces here
+/*
+
+if(!window.Peer){ // make sure PeerJS is loaded
+	(new EvalSpace()).load('//cdn.peerjs.com/latest/peer.min.js');
 }
+*/
